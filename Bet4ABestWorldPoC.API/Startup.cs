ade66de7ed609d3b75e818d8368aa33d756fe4b4
Luis Jimenez Lopez <jimenezlopez11@gmail.com>
2021-08-09 00:45:46 +0200

    added sql server to starup

--- Bet4ABestWorldPoC.API/Startup.cs
+++ Bet4ABestWorldPoC.API/Startup.cs
@@ -1,8 +1,11 @@
+using Bet4ABestWorldPoC.Repositories;
+using Bet4ABestWorldPoC.Shared.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +30,11 @@ namespace Bet4ABestWorldPoC.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options 
+                .UseSqlServer(Configuration.GetConnectionString("AppDbContext")));
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -38,8 +46,11 @@ namespace Bet4ABestWorldPoC.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext context)
         {
+
+            context.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
