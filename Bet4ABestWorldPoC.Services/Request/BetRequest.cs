b57627c6da912d9793514bad178dac40aef419fd
Luis Jimenez Lopez <jimenezlopez11@gmail.com>
2021-08-10 13:54:12 +0200

    added required attributes to requests

--- Bet4ABestWorldPoC.Services/Request/BetRequest.cs
+++ Bet4ABestWorldPoC.Services/Request/BetRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Bet4ABestWorldPoC.Services.Request
 {
     public class BetRequest
     {
+        [Required]
         public int SlotId { get; set; }
+        [Required]
         public double Amount { get; set; }
+        [Required]
         public int BetTypeId { get; set; }
 
         public BetRequest(int slotId, double amount, int betTypeId)
--- Bet4ABestWorldPoC.Services/Request/DepositRequest.cs
+++ Bet4ABestWorldPoC.Services/Request/DepositRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace Bet4ABestWorldPoC.Services.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bet4ABestWorldPoC.Services.Request
 {
     public class DepositRequest
     {
+        [Required]
         public double Amount { get; set; }
+        [Required]
         public int MerchantId { get; set; }
 
         public DepositRequest(double amount, int merchantId)
